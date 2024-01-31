1dee13d9079dd713df28703354de2b17c7eddf97
Gerard Cebria <gerard.cebrialeon@luminis.eu>
2024-01-31 15:58:29 +0100

    gui

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import cv2
 import easyocr
+import requests
 import matplotlib.pyplot as plt
 import numpy as np
 
-image_path = '/Users/gerardcebria/Desktop/MagicVision/ImageFolder/ltc/1c7ac04c-8510-468b-aa6f-f249bde9ff87.jpg'
+image_path = '/Users/gerardcebria/Desktop/MagicVision/ImageFolder/ltc/00f40514-d37d-423f-aa0c-1875300f43a8.jpg'
 
 img = cv2.imread(image_path)
 
@@ -13,18 +14,30 @@ reader = easyocr.Reader(['en'], gpu=False)
 # detect text on image
 text_ = reader.readtext(img)
 
-threshold = 0.7
+threshold = 0.2
 # draw bbox and text
+
+print(text_[0])
+text_ = [text_[0]]
 for t_, t in enumerate(text_):
-    print(t)
+    # print(t)
 
     bbox, text, score = t
 
     if score > threshold:
         rect1=(int(bbox[0][0]), int(bbox[0][1]))
         rect2=(int(bbox[2][0]), int(bbox[2][1]))
-        cv2.rectangle(img, rect1, rect2, (0, 255, 0), 5)
+        cv2.rectangle(img, rect1, rect2, (255, 0, 0), 5)
         cv2.putText(img, text, rect1, cv2.FONT_HERSHEY_COMPLEX, 0.65, (255, 0, 0), 2)
+
+        card_name = text
+        # card_name = text.replace(' ','+').replace(',','').replace('.','')
+        api_url = f"""https://api.scryfall.com/cards/named?exact={card_name}"""
+        print(api_url)
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            cv2.rectangle(img, rect1, rect2, (0, 255, 0), 5)
+        print(response.json())
 
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 plt.imshow(img)
--- reader_webcam.py
+++ reader_webcam.py
@@ -1,43 +1,90 @@
 import cv2
 import easyocr
+import requests
+import tkinter as tk
+from PIL import Image, ImageTk
 
-# instancia el lector de texto
+# Instancia el lector de texto
 reader = easyocr.Reader(['en'], gpu=True)
 
-# accede a la cámara web (en este caso, la cámara principal, 0)
+# Accede a la cámara web (en este caso, la cámara principal, 0)
 cap = cv2.VideoCapture(0)
 
-window_width = 300
+window_width = 400
 window_height = 300
+total = 0.00
 
-while True:
-    # captura el frame de la cámara
+# TODO: Implement cards already read
+cards_read = []
+
+# Función para actualizar la interfaz gráfica con el nuevo frame
+def update_gui():
+    global total
     ret, frame = cap.read()
-
-    # cambia el tamaño del frame
     frame = cv2.resize(frame, (window_width, window_height))
-
-    # detecta el texto en el frame
     text_ = reader.readtext(frame)
-
+    
     threshold = 0.7
-    # dibuja los cuadros delimitadores y el texto
+    
     for t_, t in enumerate(text_):
         bbox, text, score = t
 
         if score > threshold:
             rect1=(int(bbox[0][0]), int(bbox[0][1]))
             rect2=(int(bbox[2][0]), int(bbox[2][1]))
-            cv2.rectangle(frame, rect1, rect2, (0, 255, 0), 5)
-            cv2.putText(frame, text, rect1, cv2.FONT_HERSHEY_COMPLEX, 0.65, (255, 0, 0), 2)
+            cv2.rectangle(frame, rect1, rect2, (0, 0, 255), 5)
+            cv2.putText(frame, text, rect1, cv2.FONT_HERSHEY_COMPLEX, 0.65, (0, 0, 255), 1)
+            cv2.putText(frame, 'NOT FOUND', rect2, cv2.FONT_HERSHEY_COMPLEX, 0.65, (0, 0, 255), 1)
 
-    # muestra el frame con texto detectado
-    cv2.imshow('Text Detection', frame)
+            # if text.replace(' ','+').replace(',','').replace('.','') == card_name:
+            #     break
+            # card_name = text.replace(' ','+').replace(',','').replace('.','')
+            card_name = text
+            api_url = f"""https://api.scryfall.com/cards/named?exact={card_name}"""
+            response = requests.get(api_url)
+            if response.status_code == 200:
+                data = response.json()
+                price = float(data['prices']['eur'])
+                total = total+price
+                cv2.rectangle(frame, rect1, rect2, (0, 255, 0), 5)
+                cv2.putText(frame, 'FOUND', rect2, cv2.FONT_HERSHEY_COMPLEX, 0.65, (0, 255, 0), 1)
 
-    # sale del bucle si se presiona la tecla 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # Convierte el frame a un formato compatible con Tkinter
+    img = ImageTk.PhotoImage(Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)))
+    panel.config(image=img)
+    panel.image = img
+    
+    # Actualiza el texto en el área de texto
+    text_area.delete(1.0, tk.END)
+    text_area.insert(tk.END, f"Total Precio: {total}")
 
-# libera los recursos
-cap.release()
-cv2.destroyAllWindows()
+    # Programa una llamada recursiva después de 10 ms
+    root.after(10, update_gui)
+
+# Configuración de la interfaz gráfica
+root = tk.Tk()
+root.title("Interfaz de Webcam")
+
+# Panel para mostrar la ventana de la cámara
+panel = tk.Label(root)
+panel.pack(side="left", padx=10, pady=10)
+
+# Área de texto para mostrar información
+text_area = tk.Text(root, height=10, width=30)
+text_area.pack(side="right", padx=10, pady=10)
+
+# Inicia la actualización de la interfaz gráfica
+update_gui()
+
+# Función para detener la captura y cerrar la interfaz gráfica al presionar 'q'
+def quit_app(event):
+    if event.char == 'q':
+        cap.release()
+        cv2.destroyAllWindows()
+        root.destroy()
+
+# Enlace la función quit_app al evento de tecla 'q'
+root.bind('<Key>', quit_app)
+
+# Inicia el bucle principal de la interfaz gráfica
+root.mainloop()
